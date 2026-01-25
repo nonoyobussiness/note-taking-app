4d14a97bc25a6a0e959035cf0a424434e0171bc0
nonoyobussiness <sreevathsal2006@gmail.com>
2026-01-25 19:40:49 +0530

    added archive , delete note and search bar frontend

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,7 @@
 import type { Note } from "../App";
+import TagIcon from "./../assets/images/icon-tag.svg?react";
+import ArchiveIcon from "./../assets/images/icon-archive.svg?react";
+import DeleteIcon from "./../assets/images/icon-delete.svg?react";
 
 type EditorProps = {
     note:Note | null;
@@ -12,11 +15,32 @@ export function Editor({note,onChange}:EditorProps){
         )
     }
     return(
-        <div className="flex flex-col p-6 gap-4">
-            <input className="font-bold text-3xl text-white" value={note.title} onChange={e => onChange({...note,title:e.target.value})} ></input>
-            <textarea value={note.content} className="flex-1 bg-transparent outline-none resize-none" onChange={e=>onChange({...note,content:e.target.value})} ></textarea>
-
+        <div className="flex h-full">
+            <div className="flex flex-col p-6 gap-4 w-4/6 border-r border-slate-700 h-full ">
+                <input className="font-bold text-3xl text-white bg-transparent outline-none" value={note.title} onChange={e => onChange({...note,title:e.target.value})} ></input>
+                <div className="flex flex-row gap-3 m-3 items-center w-1/4">
+                    <TagIcon className="w-5 h-5 invert" ></TagIcon>
+                    <p className="text-neutral-400" >Tags</p>
+                    <div className="ml-auto ">
+                        {note.tags.map(tag => (
+                            <span key={tag} className="rounded-lg text-white px-2 py-1">{tag}</span>
+                        ))}
+                    </div>
+                </div>
+                <textarea value={note.content} className="flex-1 bg-transparent outline-none resize-none font-normal" onChange={e=>onChange({...note,content:e.target.value})} ></textarea>
+            </div>
+            <div className="flex-1 flex flex-col px-6 py-8 text-white font-semibold gap-4 ">
+                <button className="bg-transparent border border-slate-600 flex items-center gap-3 rounded-lg px-3 py-4" >
+                    <ArchiveIcon className="w-6 h-6 invert" ></ArchiveIcon>
+                    <p >Archive Note</p>
+                </button>
+                <button className="bg-transparent border border-slate-600 flex items-center gap-3 rounded-lg px-3 py-4" >
+                    <DeleteIcon className="w-6 h-6 invert" ></DeleteIcon>
+                    <p >Delete Note</p>
+                </button>
+            </div>
         </div>
+        
 
     )
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,5 @@
+import SearchIcon from "./../assets/images/icon-search.svg?react";
+
 type LayoutProps = {
     sidebar:React.ReactNode;
     notelist:React.ReactNode;
@@ -9,12 +11,17 @@ export function Layout({sidebar,notelist,editor}:LayoutProps){
         <div className="bg-slate-900 h-screen w-screen text-slate-100 flex">
             <aside className="h-full w-[300px] border-r border-slate-700" >{sidebar}</aside>
             <div className="flex flex-col flex-1">
-                <header className="p-10 border-b border-slate-700 ">
-                    <h1 className="text-2xl text-white font-semibold">All Notes</h1>
+                <header className="pt-9 pb-6 px-8 border-b border-slate-700 flex ">
+                    <h1 className="text-3xl text-white font-semibold">All Notes</h1>
+                    <div className="ml-auto border flex items-center border border-slate-700 px-4 py-4 gap-3 w-1/4 rounded-lg ">
+                        <SearchIcon className="w-6 h-6 invert" ></SearchIcon>
+                        <input type="text" placeholder="Search by title, content or tags..." className="outline-none bg-transparent w-full text-md " />
+
+                    </div>
                 </header>
                 <div className="flex flex-1">
-                    <div className="w-[400] border-r border-slate-700 overflow-y-auto">{notelist}</div>
-                    <div className="flex-1">{editor}</div>
+                    <div className="w-1/5 border-r border-slate-700 overflow-y-auto p-4 ">{notelist}</div>
+                    <div className="flex-1 h-full">{editor}</div>
                 </div>
             </div>
             
